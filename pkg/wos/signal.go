30d641835de358fab21c3058dcbeaaca540f61c6
zhaobin <357958137@qq.com>
2025-11-03 14:56:08 +0800

    style: sign

--- pkg/wos/signal.go
+++ pkg/wos/signal.go
@@ -9,13 +9,13 @@ import (
 
 // WaitSignal 等待系统信号
 func WaitSignal() {
-	sig := make(chan os.Signal)
+	sign := make(chan os.Signal)
 	switch runtime.GOOS {
 	case `windows`:
-		signal.Notify(sig, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
+		signal.Notify(sign, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
 	default:
-		signal.Notify(sig, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGABRT, syscall.SIGKILL, syscall.SIGTERM)
+		signal.Notify(sign, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGABRT, syscall.SIGKILL, syscall.SIGTERM)
 	}
-	<-sig
-	signal.Stop(sig)
+	<-sign
+	signal.Stop(sign)
 }
