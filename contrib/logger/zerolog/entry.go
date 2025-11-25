766f6f541a966f882cd8a99620552902ba253450
Art <357958137@qq.com>
2025-11-25 09:25:52 +0800

    feat: zerolog implement for Logger

--- contrib/logger/zerolog/entry.go
+++ contrib/logger/zerolog/entry.go
@@ -3,75 +3,75 @@ package zerolog
 import (
 	"time"
 
+	"github.com/rs/zerolog"
+
 	"github.com/byteweap/wukong/plugin/logger"
 )
 
-// Entry TODO
 type Entry struct {
+	e *zerolog.Event
 }
 
 var _ logger.Entry = (*Entry)(nil)
 
-func NewEntry() *Entry {
-	return &Entry{}
+func newEntry(e *zerolog.Event) *Entry {
+	return &Entry{e: e}
 }
 
-func (e *Entry) Str(k string, v string) logger.Entry {
-	//TODO implement me
-	panic("implement me")
+func (e *Entry) Str(k, v string) logger.Entry {
+	e.e.Str(k, v)
+	return e
 }
 
 func (e *Entry) Int64(k string, v int64) logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	e.e.Int64(k, v)
+	return e
 }
 
 func (e *Entry) Int(k string, v int) logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	e.e.Int(k, v)
+	return e
 }
 
 func (e *Entry) Uint64(k string, v uint64) logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	e.e.Uint64(k, v)
+	return e
 }
 
 func (e *Entry) Float(k string, v float64) logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	e.e.Float64(k, v)
+	return e
 }
 
 func (e *Entry) Bool(k string, v bool) logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	e.e.Bool(k, v)
+	return e
 }
 
 func (e *Entry) Time(k string, v time.Time) logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	e.e.Time(k, v)
+	return e
 }
 
 func (e *Entry) Duration(k string, v time.Duration) logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	e.e.Dur(k, v)
+	return e
 }
 
 func (e *Entry) Any(k string, v any) logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	e.e.Any(k, v)
+	return e
 }
 
 func (e *Entry) Err(err error) logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	e.e.Err(err)
+	return e
 }
 
 func (e *Entry) Msg(message string) {
-	//TODO implement me
-	panic("implement me")
+	e.e.Msg(message)
 }
 
 func (e *Entry) Msgf(format string, args ...any) {
-	//TODO implement me
-	panic("implement me")
+	e.e.Msgf(format, args...)
 }
--- contrib/logger/zerolog/logger.go
+++ contrib/logger/zerolog/logger.go
@@ -1,48 +1,106 @@
 package zerolog
 
-import "github.com/byteweap/wukong/plugin/logger"
+import (
+	"io"
+	"os"
 
-// Log TODO
+	"github.com/rs/zerolog"
+	"gopkg.in/natefinch/lumberjack.v2"
+
+	"github.com/byteweap/wukong/plugin/logger"
+)
+
 type Log struct {
+	log zerolog.Logger
 }
 
 var _ logger.Logger = (*Log)(nil)
 
-func New() *Log {
-	return &Log{}
+func New(opts ...Option) *Log {
+
+	ops := defaultOptions()
+	for _, opt := range opts {
+		opt(ops)
+	}
+
+	output := newOutput(ops)
+
+	zerolog.SetGlobalLevel(convertLevel(ops.level))
+	zerolog.TimeFieldFormat = ops.timeFormat
+	zerolog.TimestampFieldName = ops.timeFieldName
+	zerolog.LevelFieldName = ops.levelFieldName
+	zerolog.MessageFieldName = ops.messageFieldName
+
+	return &Log{
+		log: zerolog.New(output).With().Timestamp().CallerWithSkipFrameCount(3).Logger(),
+	}
 }
 
-func (l *Log) With(s string, s2 string) logger.Logger {
-	//TODO implement me
-	panic("implement me")
+func (l *Log) With(k, v string) logger.Logger {
+	return &Log{
+		log: l.log.With().Str(k, v).Logger(),
+	}
 }
 
 func (l *Log) Debug() logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	return newEntry(l.log.Debug())
 }
 
 func (l *Log) Info() logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	return newEntry(l.log.Info())
 }
 
 func (l *Log) Warn() logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	return newEntry(l.log.Warn())
 }
 
 func (l *Log) Error() logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	return newEntry(l.log.Error())
 }
 
 func (l *Log) Fatal() logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	return newEntry(l.log.Fatal())
 }
 
 func (l *Log) Panic() logger.Entry {
-	//TODO implement me
-	panic("implement me")
+	return newEntry(l.log.Panic())
+}
+
+func newOutput(ops *options) io.Writer {
+	switch ops.mode {
+	case ModeConsole:
+		return zerolog.ConsoleWriter{
+			Out:        os.Stdout,
+			TimeFormat: ops.timeFormat,
+		}
+	case ModeFile:
+		return &lumberjack.Logger{
+			Filename:   ops.fileOpts.filename,
+			MaxSize:    ops.fileOpts.maxSize,
+			MaxAge:     ops.fileOpts.maxAge,
+			MaxBackups: ops.fileOpts.maxBackups,
+			LocalTime:  ops.fileOpts.localTime,
+			Compress:   ops.fileOpts.compress,
+		}
+	}
+	return os.Stdout
+}
+
+func convertLevel(level string) zerolog.Level {
+	switch level {
+	case LevelDebug:
+		return zerolog.DebugLevel
+	case LevelInfo:
+		return zerolog.InfoLevel
+	case LevelWarn:
+		return zerolog.WarnLevel
+	case LevelError:
+		return zerolog.ErrorLevel
+	case LevelFatal:
+		return zerolog.FatalLevel
+	case LevelPanic:
+		return zerolog.PanicLevel
+	default:
+		return zerolog.DebugLevel
+	}
 }
