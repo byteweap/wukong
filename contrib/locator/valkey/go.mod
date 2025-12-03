4434b3ec6ebbf0d44aa6cd683245f4938aa339e6
Art <357958137@qq.com>
2025-12-03 16:40:24 +0800

    feat: valkey implement for locator

---
+++ contrib/locator/valkey/go.mod
@@ -0,0 +1,7 @@
+module github.com/byteweap/wukong/contrib/locator/valkey
+
+go 1.25.4
+
+replace github.com/byteweap/wukong => ../../../
+
+require github.com/byteweap/wukong v0.0.0-20251125083652-5c96ecfa5ded
---
+++ contrib/locator/valkey/locator.go
@@ -0,0 +1,48 @@
+package valkey
+
+import (
+	"context"
+
+	"github.com/byteweap/wukong/plugin/locator"
+)
+
+type Locator struct {
+	// Add fields here
+}
+
+var _ locator.Locator = (*Locator)(nil)
+
+func (l *Locator) ID() string {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *Locator) Gate(ctx context.Context, uid int64) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *Locator) BindGate(ctx context.Context, uid int64, node string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *Locator) UnBindGate(ctx context.Context, uid int64, node string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *Locator) Game(ctx context.Context, uid int64) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *Locator) BindGame(ctx context.Context, uid int64, node string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *Locator) UnBindGame(ctx context.Context, uid int64, node string) error {
+	//TODO implement me
+	panic("implement me")
+}
